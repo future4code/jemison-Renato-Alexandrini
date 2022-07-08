d14d1a24237b52dc4251474160d9f1bc2e6f0da5
RenatoAlexandrini <re.alexandrini@gmail.com>
2022-07-07 22:16:49 -0300

    Exercicio aula 2 de renderizacao de listas

--- modulo3/aula-estados-inta4/insta4/src/App.js
+++ modulo3/aula-estados-inta4/insta4/src/App.js
@@ -1,43 +1,94 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components'
 import Post from './components/Post/Post';
 
 const MainContainer = styled.div`
   display: flex;
   justify-content: center;
-  flex-direction: column;
-  align-items: center;
-`
+  padding: 15px;
+  margin:30px;
+  `
 
 function App() {
-  const postagemCompleta =[
-    {nome:'paulinha',
+  const [inputNome, setInputNome] = useState("")
+  const [inputfoto, setInputFoto] = useState("")
+  const [inputImagem, setInputImagem] = useState("")
+  const [postagem, setPost] = useState([
+
+    {nome:'Renato Alexandrini',
     foto:'https://picsum.photos/50/50',
     imagem:'https://picsum.photos/200/150'},
-    
-    {nome:'renato',
-    foto:'https://picsum.photos/51/51',
-    imagem:'https://picsum.photos/201/151'},
-    
-    {nome:'alexandrini',
-    foto:'https://picsum.photos/52/52',
-    imagem:'https://picsum.photos/202/152'}
-  ]
+   ])
+  
+  const handleInputNome = (e) => {
+    setInputNome(e.target.value)
+  }
 
-  const novoArrayPosts = postagemCompleta.map((item, index) => {
+  const handleInputFoto = (e) => {
+    setInputFoto(e.target.value)
+  }
+
+  const handleInputImagem = (e) => {
+    setInputImagem(e.target.value)
+  }
+
+  // adicionar item
+  const addPostagem = (e) => {
+    e.preventDefault();
+
+    const novoPost = {nome: inputNome, foto: inputfoto, imagem: inputImagem}
+    const novaListaDePosts = [...postagem, novoPost]
+    setPost(novaListaDePosts)
+  }
+
+
+  const novoArrayPosts = postagem.map((item, index) => {
+    const deletarPost = () => {
+      const novaListaDePosts = [...postagem]
+      const deletar = novaListaDePosts.findIndex((postExcluido) => {
+        return postExcluido === postagem
+      })
+      novaListaDePosts.splice(deletar, 1)
+      setPost(novaListaDePosts)
+    }
     return (
+      <main>
       <Post key={index}
         nomeUsuario = {item.nome}
         fotoUsuario = {item.foto}
         fotoPost ={item.imagem}
-      />
+        />
+      <button onClick={deletarPost}>X</button>
+    </main>
     )
 
   })
 return(
+  <main>
   <MainContainer>
-      {novoArrayPosts}    
-        </MainContainer>
+      <label>Nome:</label>
+        <input
+          placeholder='Insira um nome'
+          value={inputNome}
+          onChange={handleInputNome}
+        />
+        
+        <label>Foto:</label>
+        <input
+          placeholder='Insira sua Foto'
+          value={inputfoto}
+          onChange={handleInputFoto}
+        />
+         <label>Imagem:</label>
+        <input
+          placeholder='Insira sua Imagem'
+          value={inputImagem}
+          onChange={handleInputImagem}
+        />
+        <button onClick={addPostagem}>Adicionar</button>
+      </MainContainer>
+      {novoArrayPosts}
+       </main>
 )
 }
 
