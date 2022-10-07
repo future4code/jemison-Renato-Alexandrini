d23de87bb0ade1861f9ec37ce88e8992b64ab23d
RenatoAlexandrini <re.alexandrini@gmail.com>
2022-10-06 23:00:39 -0300

    continuação dos exercicios

--- modulo5/revisao-javascript-backend/revisao-javascript/exercicio1/index.js
+++ modulo5/revisao-javascript-backend/revisao-javascript/exercicio1/index.js
@@ -1,18 +1,25 @@
 
 const value = 0
-const letter =''
-const converted = 0
+const letter = ''
+let converted = 0
 
-const TemperatureConverter =(value, letter)=>{
-if(typeof(value) != 'number'){
-     return console.log("A temperatura precisa ser um valor númerico")
-}else if(typeof(value) === 'number' && toUpperCase(letter) === 'F') {
-converted = (value * 1.8) + 32
-return console.log(`${value}º graus Celsius é equivalente a ${converted}º Farenheit`)
-}else if(typeof(value) === 'number' && toUpperCase(letter) === 'K') {
-     converted = (value + 273) 
-     return console.log(`${value}º graus Celsius é equivalente a ${converted}º Kelvin`)
-}
+const TemperatureConverter = (value, letter) => {
+
+     if (typeof (value) != 'number') {
+          return console.log("A temperatura precisa ser um valor númerico")
+     } else if ((letter).toUpperCase() === 'F') {
+      converted = (value * 1.8) + 32
+     
+          return console.log(`${value}º graus Celsius é equivalente a ${Math.round(converted)}º Fahrenheit`)
+     } else if ((letter).toUpperCase() === 'K') {
+          converted = value + 273
+          return console.log(`${value}º graus Celsius é equivalente a ${converted}º Kelvin`)
+     }else{console.log('É necessário escolher entre F - Fahrenheit ou K - Kelvin para converter o seu valor')}
 }
 
-TemperatureConverter(20, k)
+
+TemperatureConverter(25.3, 'f')
+TemperatureConverter(30, 'k')
+TemperatureConverter(20,'x')
+TemperatureConverter('vinte', 'f')
+TemperatureConverter('vinte', 'k')
--- modulo5/revisao-javascript-backend/revisao-javascript/exercicio3/index.js
+++ modulo5/revisao-javascript-backend/revisao-javascript/exercicio3/index.js
@@ -1,1 +1,29 @@
-console.log("exercicio 3");
+
+
+
+const contas = [
+	{
+		email: "astrodev@labenu.com",
+		password: "abc123"
+	},
+	{
+		email: "re.alexandrini@gmail.com",
+		password: "123456"
+	}
+]
+
+const Login = (email, password) =>{
+
+    validEmail = /[a-zA-Z0-9]+@[a-z]{3}[.a-z]?/.test(email);
+    validPassword = /.{6,}/.test(password);
+
+    if(validEmail != true){
+console.log('Preencha corretamente o email')
+    }
+    if(validPassword != true){
+        console.log('Preencha corretamente o email')
+            }
+if(contas.includes())
+} 
+
+Login('', '')
