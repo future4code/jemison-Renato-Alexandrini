7899fab4df153f01ad2f71acaf3f69a85811ebfb
RenatoAlexandrini <re.alexandrini@gmail.com>
2022-10-07 03:14:28 -0300

    finalizacao revisão javascript

--- modulo5/revisao-javascript-backend/revisao-javascript/exercicio3/index.js
+++ modulo5/revisao-javascript-backend/revisao-javascript/exercicio3/index.js
@@ -12,18 +12,46 @@ const contas = [
 	}
 ]
 
-const Login = (email, password) =>{
+const Login = (newEmail, newPassword) => {
 
-    validEmail = /[a-zA-Z0-9]+@[a-z]{3}[.a-z]?/.test(email);
-    validPassword = /.{6,}/.test(password);
+	let validEmailArray = []
+	let validPasswordArray = []
+	let validator = false
 
-    if(validEmail != true){
-console.log('Preencha corretamente o email')
-    }
-    if(validPassword != true){
-        console.log('Preencha corretamente o email')
-            }
-if(contas.includes())
-} 
+	validEmail = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]{3}?/.test(newEmail);
+	validPassword = /.{6,}/.test(newPassword);
 
-Login('', '')
+	if (validEmail == false) {
+		console.log('Preencha corretamente o email')
+	}
+	else {
+		contas.forEach((account) => {
+			validEmailArray.push(account.email === newEmail)
+		})
+	}
+	if (validPassword == false) {
+		console.log('A senha deve conter no mínimo 6 caracteres')
+	}
+	else {
+		contas.forEach((account) => {
+			validPasswordArray.push(account.password === newPassword)
+		})
+	}
+
+	if (validEmailArray.includes(true) && validEmail === true && validPasswordArray.includes(true) && validPassword === true) {
+		return console.log('Login Bem sucedido')
+	}
+	else if (validEmailArray.includes(false) && validEmail === true && validPasswordArray.includes(false) && validPassword === true) {
+		return console.log('email ou senha inválidos')
+	}
+}
+
+Login('astrodev@labenu.com', 'abc123')
+Login('re.alexandrini@gmail.com', '123456')
+
+Login('email@errado.com', '123456')
+Login('astrodev@labenu.com','senhaErrada')
+
+Login('emailInvalido','abc123')
+Login('re.alexandrini@gmail.com','123')
+
--- modulo5/revisao-javascript-backend/revisao-javascript/exercicio4/exercicio.js
+++ modulo5/revisao-javascript-backend/revisao-javascript/exercicio4/exercicio.js
@@ -1,1 +1,31 @@
-console.log("exercicio 4");
+
+
+let lista = [
+    {
+        nome: "Banana"
+    },
+    {
+        nome: "Laranja"
+    },
+    {
+        nome: "Cebola"
+    }
+]
+let newArray = []
+const UnicList = (secondArray) => {
+    let arrayJunction = [...lista, ...secondArray]
+arrayJunction.forEach((item)=>{
+    let duplicatedItem = newArray.findIndex(dupItem =>{
+        return item.nome == dupItem.nome }) > -1
+    if(!duplicatedItem){
+        newArray.push(item)
+    }
+})
+ console.log(newArray)
+}
+
+let list2 = [{ nome: 'Banana' }, { nome: 'Acerola' }, { nome: 'Cebola' }, { nome: 'Jabuticaba' }, { nome: 'Banana' }, { nome: 'Cenoura' }]
+UnicList(list2)
+
+let list3 = [{ nome: 'Couve' }, { nome: 'Repolho' }, { nome: 'Manga' }, { nome: 'Jabuticaba' }, { nome: 'Banana' }, { nome: 'Banana' }, { nome: 'Banana' },{ nome: 'Banana' },{ nome: 'Cebola' }]
+UnicList(list3)
