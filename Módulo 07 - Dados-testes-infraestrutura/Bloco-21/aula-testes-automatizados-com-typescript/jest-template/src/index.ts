3a2491ead90f11615e39832e75318b7764516c18
RenatoAlexandrini <re.alexandrini@gmail.com>
2023-02-28 22:05:33 -0300

    exercio-testes-automatizados inconpleto

---
+++ Módulo 07 - Dados-testes-infraestrutura/Bloco-21/aula-testes-automatizados-com-typescript/jest-template/src/index.ts
@@ -0,0 +1,15 @@
+import {User} from './interface/interface';
+
+function performPurchase(user: User, value: number): User | undefined {
+	if(user.balance >= value) {
+		return {
+			...user,
+			balance: user.balance - value		
+		}
+	}
+	return undefined
+}
+
+let user
+let value
+performPurchase(user, value)
